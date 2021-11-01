0a37a76f521051f3df34dea24192add5db32237b
Phamin Promcharoen <nice.phamin@gmail.com>
2021-11-01 10:50:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: false }))
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// css/ of server
+app.use(express.static(path.join(__dirname, 'server/public')))
+// pick fonts from client sourcecodes
+app.use(express.static(path.join(__dirname, 'client/src/assets/fonts')))
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+const port = process.env.PORT || 3000
+app.listen(port)
+
+console.log(`GOMO Container listening on ${port}`)
